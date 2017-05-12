f5b335cefae1dd9f87574a12cb5d6f9dd05a4b3f
arieftb <turbagus14@gmail.com>
2017-05-12 23:37:05 +0700

    Sampe Login

---
+++ app/src/main/java/arieftb/liverpoolday/LoginActivity.java
@@ -0,0 +1,55 @@
+package arieftb.liverpoolday;
+
+import android.content.Intent;
+import android.support.annotation.StringDef;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    ImageView ivLogoApp;
+    EditText edUsername, edPassword;
+    Button btnLogin, btnRegister;
+
+    private final String userName = "admin";
+    private final String passWord = "admin";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        getSupportActionBar().setTitle("Login");
+
+        ivLogoApp = (ImageView) findViewById(R.id.iv_logoApp);
+        edUsername = (EditText) findViewById(R.id.ed_username);
+        edPassword = (EditText) findViewById(R.id.ed_password);
+        btnLogin = (Button) findViewById(R.id.btn_login);
+        btnRegister = (Button) findViewById(R.id.btn_register);
+
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if((edUsername.getText().toString().equals("")) ||  (edPassword.getText().toString().equals(""))) {
+                    Toast.makeText(getApplicationContext(), "Isian Belum Lengkap", Toast.LENGTH_LONG).show();
+                }
+                else if((edUsername.getText().toString().equals(userName)) ||  (edPassword.getText().toString().equals(passWord))){
+                    Toast.makeText(getApplicationContext(), "Login Berhasil", Toast.LENGTH_LONG).show();
+//                    Intent mIntent = new Intent(getApplicationContext(), MenuActivity.class);
+//                    startActivity(mIntent);
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), "Login Gagal", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+    }
+}
--- app/src/main/java/arieftb/liverpoolday/SplashActivity.java
+++ app/src/main/java/arieftb/liverpoolday/SplashActivity.java
@@ -1,13 +1,38 @@
 package arieftb.liverpoolday;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
+
+    TextView tvNameApp;
+    ImageView ivLogoApp;
+    ProgressBar pbLoadApp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        getSupportActionBar().hide();
+
+        tvNameApp = (TextView) findViewById(R.id.tv_nameApp);
+        ivLogoApp = (ImageView) findViewById(R.id.iv_logoApp);
+        pbLoadApp = (ProgressBar) findViewById(R.id.pb_loadApp);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mIntent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(mIntent);
+                finish();
+            }
+        },3000);
     }
 }
